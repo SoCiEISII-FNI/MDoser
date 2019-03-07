ab5d813cd5d1002f9375df6d7bc327c13ddf95ca
Kevin <kevin.sanchezpeep@gmail.com>
2019-03-06 23:30:33 -0800

    interfaces added missing link to calculus and improve splash

---
+++ app/src/main/java/com/soces_fni/mdoser/SectionsPageAdapter.java
@@ -0,0 +1,40 @@
+package com.soces_fni.mdoser;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionsPageAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragmentList = new ArrayList<>();
+    private List<String> titleList = new ArrayList<>();
+
+    public void addFragment(Fragment fragment, String title)
+    {
+        fragmentList.add(fragment);
+        titleList.add(title);
+    }
+
+    public SectionsPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
